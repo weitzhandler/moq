18f682adbf5a6b82614fca10561e6e16007b7000
Daniel Cazzulino <daniel@cazzulino.com>
2018-09-22 23:40:04 -0300

    Experiment with including the API via targets
    Instead of package content files, that are visible in the solution.
    Turns out that even if the actual files aren't visible by default,
    the folder still is, so not great :(

---
+++ src/Moq/Moq.CodeFix/Api/vb/netstandard2.0/Mocks/Mock.vb
@@ -0,0 +1,22 @@
+﻿Option Strict On
+
+Imports System.CodeDom.Compiler
+Imports System.Reflection
+Imports Moq.Sdk
+
+Namespace Global.Moq
+
+    <GeneratedCode("Stunts", "5.0")>
+    Partial Friend Class Mock
+
+        Private Shared Function Create(Of T)(ByVal behavior As MockBehavior, ByVal constructorArgs As Object(), ParamArray interfaces As Type()) As T
+            Dim mocked = DirectCast(MockFactory.[Default].CreateMock(GetType(Mock).GetTypeInfo().Assembly, GetType(T), interfaces, constructorArgs), IMocked)
+
+            mocked.SetBehavior(behavior)
+
+            Return DirectCast(mocked, T)
+        End Function
+
+    End Class
+
+End Namespace
